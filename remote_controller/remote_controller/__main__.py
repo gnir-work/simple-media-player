62d834f9d7dfd4d4423df0931026dd35b28a8bad
Nir Geller <gnir.work@gmail.com>
2022-02-05 04:07:49 -0800

    Added stdout logger handler.

--- remote_controller/remote_controller/__main__.py
+++ remote_controller/remote_controller/__main__.py
@@ -1,6 +1,8 @@
+import sys
 from pathlib import Path
 
 from evdev import InputDevice
+from logbook import Logger, StreamHandler
 
 from remote_controller.commands.change_episode import ChangeEpisode
 from remote_controller.commands.power_off import PowerOff
@@ -9,13 +11,12 @@ from remote_controller.commands.toggle_play import TogglePlay
 from remote_controller.ir_reciever import get_single_remote_key
 from remote_controller.utils import get_gpio_device
 from vlc_controller.series_player import SeriesPlayer
-from logbook import Logger
 
 logger = Logger('IR_MAIN')
 COMMANDS = [ChangeEpisode, TogglePlay, RewindPlayer, PowerOff]
 
 
-def main_loop(device: InputDevice, player: SeriesPlayer):
+def loop(device: InputDevice, player: SeriesPlayer):
     while True:
         logger.info("Waiting for next IR command")
         ir_data = get_single_remote_key(device)
@@ -26,11 +27,24 @@ def main_loop(device: InputDevice, player: SeriesPlayer):
                 command.execute(ir_data, device, player)
 
 
-if __name__ == "__main__":
+def setup_loggers():
+    StreamHandler(sys.stdout).push_application()
+
+
+def main():
+    setup_loggers()
     logger.info("Started main IR loop...")
     device = get_gpio_device()
     logger.info(f"Found IR device {device.name}")
     player = SeriesPlayer(
         Path("/home/pi/Code/simple-media-player/vlc_controller/tests/test_episodes")
     )
-    main_loop(device, player)
+    loop(device, player)
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception:
+        logger.exception()
+        raise
