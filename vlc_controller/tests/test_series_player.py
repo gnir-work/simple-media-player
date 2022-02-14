569c9f1e423f283780ded284c528e5d06d5933ae
Nir Geller <gnir.work@gmail.com>
2022-02-14 09:16:04 +0200

    Added failing test for playing a folder.

--- vlc_controller/tests/test_series_player.py
+++ vlc_controller/tests/test_series_player.py
@@ -12,3 +12,11 @@ def test_play_by_number(series_player, episode_name, episode_number):
     series_player.play_episode(episode_number)
     time.sleep(VLC_SETUP_TIME)
     assert check_if_file_is_open(episode_name), "First episode wasn't played!"
+
+
+def test_play_folder(capfd, series_player):
+    folder_index = 3
+    series_player.play_episode(folder_index)
+    time.sleep(VLC_SETUP_TIME)
+    error_msg = capfd.readouterr().err
+    assert "libdvdread: Can't open file VIDEO_TS.IFO." in error_msg
