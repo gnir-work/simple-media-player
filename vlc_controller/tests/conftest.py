ffc34dc9b5c3d0c640fbd2807b1abaa012347534
Nir Geller <gnir.work@gmail.com>
2022-02-14 09:26:06 +0200

    Updates test to new code.

--- vlc_controller/tests/conftest.py
+++ vlc_controller/tests/conftest.py
@@ -1,14 +1,14 @@
 import sys
+from os import listdir
 from pathlib import Path
 
 from logbook import StreamHandler
 from pytest import fixture, yield_fixture
-
 from tests.consts import EPISODES_FOLDER
 from vlc_controller.series_player import SeriesPlayer
 
 
-@fixture
+@fixture(scope="session")
 def episodes_folder():
     return Path(__file__).parent / EPISODES_FOLDER
 
@@ -23,3 +23,10 @@ def series_player(episodes_folder):
 @fixture(scope="session", autouse=True)
 def setup_logger():
     StreamHandler(sys.stdout).push_application()
+
+
+@fixture(scope="session")
+def folder_index(episodes_folder: Path):
+    episodes = listdir(episodes_folder.as_posix())
+    folder_name = "should_be_ignored"
+    return episodes.index(folder_name) + 1
--- vlc_controller/tests/test_series_player.py
+++ vlc_controller/tests/test_series_player.py
@@ -14,9 +14,6 @@ def test_play_by_number(series_player, episode_name, episode_number):
     assert check_if_file_is_open(episode_name), "First episode wasn't played!"
 
 
-def test_play_folder(capfd, series_player):
-    folder_index = 3
-    series_player.play_episode(folder_index)
-    time.sleep(VLC_SETUP_TIME)
-    error_msg = capfd.readouterr().err
-    assert "libdvdread: Can't open file VIDEO_TS.IFO." in error_msg
+def test_play_folder(folder_index, series_player):
+    with pytest.raises(IndexError):
+        series_player.play_episode(folder_index)
