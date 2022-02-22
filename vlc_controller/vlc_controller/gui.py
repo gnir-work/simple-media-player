9a85046751e5effc318e77d6d27654f4e5bb5a8b
Nir Geller <gnir.work@gmail.com>
2022-02-22 11:49:22 -0800

    Code fixups

--- vlc_controller/vlc_controller/gui.py
+++ vlc_controller/vlc_controller/gui.py
@@ -2,15 +2,16 @@ import PySimpleGUI as sg
 
 
 def popup_message(message: str, timeout: int):
+    """
+    A small GUI popup message.
+    @param message: The message string
+    @param timeout: The timeout before the message disappears in seconds.
+    """
     layout = [[sg.Text(message)]]
-    window = sg.Window("Demo", layout, no_titlebar=True, keep_on_top=True, font=('Arial', 35, "italic"))
+    window = sg.Window("Popup", layout, no_titlebar=True, keep_on_top=True, font=('Arial', 35, "italic"))
 
     while True:
         _, _ = window.read(timeout=1000 * timeout)
         break
 
     window.close()
-
-
-if __name__ == '__main__':
-    popup_message("вы просматриваете эпизод 10", 5)
